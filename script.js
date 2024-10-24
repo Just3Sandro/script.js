130dcb3a021989d639e49f40a6c3bdaf415b38f8
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 18:11:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const word = document.getElementById("word");
-const res = document.getElementById("result");
-
-word.oninput = () => {
-   document.location.hash = word.value;
-}
+alert(document.cookie)

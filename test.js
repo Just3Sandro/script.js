da367af713108f0adaa44c97e627f0e94862cbf0
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 09:18:19 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-alert("yo");
+const button = document.createElement('button');
+    button.id = 'myButton';
+    button.innerText = 'Cliquez-moi !';
+    
+    // Ajout de l'événement de clic
+    button.addEventListener('click', function() {
+        alert('Bouton cliqué !');

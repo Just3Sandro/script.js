f5a4b21b24583e6915f13053dd0f2edf63b56c85
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 12:53:53 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
   const img = document.createElement('img');
   img.id = 'myImg';
-  img.src = '../reverse.PNG/img.jpg'; // Replace with your image source
+  img.src = '../reverese.PNG/img.jpg'; // Replace with your image source
   
   // Append the image to the body (or another container)
   document.body.appendChild(img);
+  alert('5');
   
   // Set the onerror handler
   img.onerror = function() {

